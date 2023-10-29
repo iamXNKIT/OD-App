416a61fe26bec7811f3848ca85f62b522cf85648
Ankit Pande <arprocks1583@gmail.com>
2023-10-29 12:27:00 +0530

    initial commit

---
+++ app/src/main/java/com/ankit/objectdetector/activity/MainActivity.kt
@@ -0,0 +1,193 @@
+package com.ankit.objectdetector.activity
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import androidx.databinding.DataBindingUtil
+import com.ankit.objectdetector.R
+import com.ankit.objectdetector.databinding.ActivityMainBinding
+import com.ankit.objectdetector.ml.SsdMobilenetV11Metadata1
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import java.lang.reflect.Method
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraManager: CameraManager
+    private lateinit var handler:Handler
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var bitmap: Bitmap
+    private lateinit var model:SsdMobilenetV11Metadata1
+    private lateinit var imageProcessor: ImageProcessor
+    private lateinit var labels:List<String>
+    val paint = Paint()
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val model = SsdMobilenetV11Metadata1.newInstance(this)
+        getPermission()
+        labels = FileUtil.loadLabels(this,"label.txt")
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300,300,ResizeOp.ResizeMethod.BILINEAR)).build()
+
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        binding.textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+                bitmap = binding.textureView.bitmap!!
+
+
+
+                // Creates inputs for reference.
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+                // Runs model inference and gets result.
+                val outputs = model.process(image)
+                val locations = outputs.locationsAsTensorBuffer.floatArray
+                val classes = outputs.classesAsTensorBuffer.floatArray
+                val scores = outputs.scoresAsTensorBuffer.floatArray
+                val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
+
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888,true)
+                val canvas = Canvas(mutable)
+
+                val h = mutable.height
+                val w = mutable.width
+                paint.textSize = h/15f
+                paint.strokeWidth = h/85f
+                var x = 0
+                scores.forEachIndexed { index, fl ->
+                    x = index
+                    x *= 4
+                    if(fl > 0.5){
+                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(RectF(locations.get(x+1)*w, locations.get(x)*h, locations.get(x+3)*w, locations.get(x+2)*h), paint)
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(labels.get(classes.get(index).toInt())+" "+fl.toString(), locations.get(x+1)*w, locations.get(x)*h, paint)
+                    }
+                }
+
+                binding.imageView.setImageBitmap(mutable)
+
+            }
+
+        }
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+
+    }
+
+    private fun openCamera() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            cameraManager.openCamera(cameraManager.cameraIdList[0],
+                @SuppressLint("MissingPermission")
+                object:CameraDevice.StateCallback(){
+                    override fun onOpened(camera: CameraDevice) {
+                        cameraDevice = camera
+                        var surfaceTexture = binding.textureView.surfaceTexture
+                        var surface = Surface(surfaceTexture)
+                        var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                        captureRequest.addTarget(surface)
+
+                        cameraDevice.createCaptureSession(listOf(surface),object:CameraCaptureSession.StateCallback(){
+                            override fun onConfigured(session: CameraCaptureSession) {
+                                session.setRepeatingRequest(captureRequest.build(),null,null)
+                            }
+
+                            override fun onConfigureFailed(session: CameraCaptureSession) {
+                                TODO("Not yet implemented")
+                            }
+
+                        },handler)
+                    }
+
+                    override fun onDisconnected(camera: CameraDevice) {
+
+                    }
+
+                    override fun onError(camera: CameraDevice, error: Int) {
+
+                    }
+
+                },handler)
+        }else{
+            getPermission()
+        }
+
+    }
+
+    private fun getPermission() {
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA),101)
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermission()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
+}
